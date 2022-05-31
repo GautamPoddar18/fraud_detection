728f771abc233d5ecaa686fcf9d82d51ad283ab9
GautamPoddar18 <Gautam.34Pr@gmail.com>
2022-05-31 16:56:25 +0530

    added 3 new ML model code

--- ml_model.py
+++ ml_model.py
@@ -5,3 +5,14 @@ Created on Tue May 31 16:22:43 2022
 @author: GAUTAM PODDAR
 """
 print('Adding all ML Related Code Hers')
+
+def model1():
+    print('Adding code for model 1')
+    
+def model2():
+    print('Adding code for model 2')
+
+def model3():
+    print('Adding code for model 3')
+    
+
